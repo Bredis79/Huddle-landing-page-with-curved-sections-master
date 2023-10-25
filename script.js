ffb9179c7928454967dc05f5b883b2a906ee51ec
Bredis <77401763+Bredis79@users.noreply.github.com>
2023-10-25 21:51:55 +0200

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//select input
+let input = document.querySelector("#footer__input")
+
+//select submit button
+let submitBtn = document.querySelector("#footer__submit")
+
+//create new paragraph 
+let newParagraph = document.createElement("p")
+
+//set class name to newParagraph
+newParagraph.className = "test"
+
+//set value to the paragraph
+newParagraph.textContent = "Check your email please" 
+
+//select right side of the footer
+let rightSide = document.querySelector(".footer__right_side")
+
+//check if email address is valid
+submitBtn.addEventListener("click", function (event){
+    
+    if(input.value.includes('@') === false || input.value.includes(".") === false) {
+        event.preventDefault()
+        rightSide.appendChild(newParagraph)
+        newParagraph.style.color = "#ff4242"
+        newParagraph.style.fontSize = "0.8rem"
+        input.style.border = "1px solid #ff4242"
+        input.style.cursor = "pointer"
+    } 
+
+    if(window.innerWidth < 1201 || window.innerWidth > 481) {
+        newParagraph.style.marginTop = "-6rem"
+    } 
+    
+    if(window.innerWidth <= 481) {
+        newParagraph.style.marginTop = "-4rem"
+    }
+   
+    //form.reset()
+     alert(window.innerWidth)
+    })
+   
+
+
